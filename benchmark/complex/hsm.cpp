bf3b9d523e0ba265b503cf87de52aea28195922b
Erik Zenker <erikzenker@posteo.de>
2020-05-05 20:29:15 +0200

    Initial commit

---
+++ benchmark/complex/hsm.cpp
@@ -0,0 +1,117 @@
+#include "ComplexStateMachine.h"
+#include "benchmark.hpp"
+#include <hsm/hsm.h>
+
+using namespace hsm;
+
+
+int main()
+{
+    hsm::sm<ComplexStateMachine> sm;
+
+    benchmark_execution_speed([&] {
+        for (auto i = 0; i < 1'000'000; ++i) {
+            if (rand() % 2)
+                sm.process_event(e1());
+            if (rand() % 2)
+                sm.process_event(e2());
+            if (rand() % 2)
+                sm.process_event(e3());
+            if (rand() % 2)
+                sm.process_event(e4());
+            if (rand() % 2)
+                sm.process_event(e5());
+            if (rand() % 2)
+                sm.process_event(e6());
+            if (rand() % 2)
+                sm.process_event(e7());
+            if (rand() % 2)
+                sm.process_event(e8());
+            if (rand() % 2)
+                sm.process_event(e9());
+            if (rand() % 2)
+                sm.process_event(e10());
+            if (rand() % 2)
+                sm.process_event(e11());
+            if (rand() % 2)
+                sm.process_event(e12());
+            if (rand() % 2)
+                sm.process_event(e13());
+            if (rand() % 2)
+                sm.process_event(e14());
+            if (rand() % 2)
+                sm.process_event(e15());
+            if (rand() % 2)
+                sm.process_event(e16());
+            if (rand() % 2)
+                sm.process_event(e17());
+            if (rand() % 2)
+                sm.process_event(e18());
+            if (rand() % 2)
+                sm.process_event(e19());
+            if (rand() % 2)
+                sm.process_event(e20());
+            if (rand() % 2)
+                sm.process_event(e21());
+            if (rand() % 2)
+                sm.process_event(e22());
+            if (rand() % 2)
+                sm.process_event(e23());
+            if (rand() % 2)
+                sm.process_event(e24());
+            if (rand() % 2)
+                sm.process_event(e25());
+            if (rand() % 2)
+                sm.process_event(e26());
+            if (rand() % 2)
+                sm.process_event(e27());
+            if (rand() % 2)
+                sm.process_event(e28());
+            if (rand() % 2)
+                sm.process_event(e29());
+            if (rand() % 2)
+                sm.process_event(e30());
+            if (rand() % 2)
+                sm.process_event(e31());
+            if (rand() % 2)
+                sm.process_event(e32());
+            if (rand() % 2)
+                sm.process_event(e33());
+            if (rand() % 2)
+                sm.process_event(e34());
+            if (rand() % 2)
+                sm.process_event(e35());
+            if (rand() % 2)
+                sm.process_event(e36());
+            if (rand() % 2)
+                sm.process_event(e37());
+            if (rand() % 2)
+                sm.process_event(e38());
+            if (rand() % 2)
+                sm.process_event(e39());
+            if (rand() % 2)
+                sm.process_event(e40());
+            if (rand() % 2)
+                sm.process_event(e41());
+            if (rand() % 2)
+                sm.process_event(e42());
+            if (rand() % 2)
+                sm.process_event(e43());
+            if (rand() % 2)
+                sm.process_event(e44());
+            if (rand() % 2)
+                sm.process_event(e45());
+            if (rand() % 2)
+                sm.process_event(e46());
+            if (rand() % 2)
+                sm.process_event(e47());
+            if (rand() % 2)
+                sm.process_event(e48());
+            if (rand() % 2)
+                sm.process_event(e49());
+            if (rand() % 2)
+                sm.process_event(e50());
+        }
+    });
+    benchmark_memory_usage(sm);
+}
---
+++ benchmark/simple/hsm.cpp
@@ -0,0 +1,93 @@
+//
+// Copyright (c) 2016-2019 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "benchmark.hpp"
+#include <hsm/hsm.h>
+    
+using namespace hsm;
+
+struct Empty{};
+struct Open{};
+struct Stopped{};
+struct Playing{};
+struct Pause{};
+
+struct play {};
+struct end_pause {};
+struct stop {};
+struct pause2 {
+};
+struct open_close {};
+struct cd_detected {};
+
+auto start_playback = [](auto, auto, auto) {};
+auto resume_playback = [](auto, auto, auto) {};
+auto close_drawer = [](auto, auto, auto) {};
+auto open_drawer = [](auto, auto, auto) {};
+auto stop_and_open = [](auto, auto, auto) {};
+auto stopped_again = [](auto, auto, auto) {};
+auto store_cd_info = [](auto, auto, auto) {};
+auto pause_playback = [](auto, auto, auto) {};
+auto stop_playback = [](auto, auto, auto) {};
+
+struct player {
+    static constexpr auto make_transition_table()
+    {
+
+        // clang-format off
+    return transition_table(
+        state<Stopped> {} + event<play>{}        /  start_playback  = state<Playing> {},
+        state<Pause> {}   + event<end_pause>{}   /  resume_playback = state<Playing> {},
+        state<Open> {}    + event<open_close>{}  /  close_drawer    = state<Empty> {},
+        state<Empty> {}   + event<open_close>{}  /  open_drawer     = state<Open> {},
+        state<Pause> {}   + event<open_close>{}  /  stop_and_open   = state<Open> {},
+        state<Stopped> {} + event<open_close>{}  /  open_drawer     = state<Open> {},
+        state<Playing> {} + event<open_close>{}  /  stop_and_open   = state<Open> {},
+        state<Playing> {} + event<pause2>{}      /  pause_playback  = state<Pause> {},
+        state<Playing> {} + event<stop>{}        /  stop_playback   = state<Stopped> {},
+        state<Pause> {}   + event<stop>{}        /  stop_playback   = state<Stopped> {},
+        state<Empty> {}   + event<cd_detected>{} /  store_cd_info   = state<Stopped> {},
+        state<Stopped> {} + event<stop>{}        /  stopped_again   = state<Stopped> {}
+    );
+        // clang-format on
+    }
+
+  static constexpr auto initial_state(){
+      return initial(state<Empty> {});
+  }
+
+};
+
+int main() {
+  hsm::sm<player> sm;
+
+  auto a = open_close{};  
+  auto b = cd_detected{};
+  auto c = play{};
+  auto d = pause2 {};
+  auto e = stop{};
+  auto f = end_pause{};
+
+  benchmark_execution_speed([&] {
+    for (auto i = 0; i < 1'000'000; ++i) {
+      sm.process_event(a);
+      sm.process_event(a);
+      sm.process_event(b);
+      sm.process_event(c);
+      sm.process_event(d);
+      // go back to Playing
+      sm.process_event(f);
+      sm.process_event(d);
+      sm.process_event(e);
+      // event leading to the same state
+      sm.process_event(e);
+      sm.process_event(a);
+      sm.process_event(a);
+    }
+  });
+  benchmark_memory_usage(sm);
+}
